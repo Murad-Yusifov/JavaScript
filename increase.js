12c0088ed97e8f69a29020e7b9b9f3a3ba3e5762
Murad-Yusifov <muradmy-azmp203@code.edu.az>
2025-04-02 09:53:13 +0400

    commit it

--- increase.js
+++ increase.js
@@ -6,7 +6,7 @@ const btn3 = document.getElementById("btn3");
 const btn4 = document.getElementById("btn4");
 
 btn.addEventListener("click", function () {
-  num.textContent = Number(num.textContent) - 1;
+    num.textContent = num.textContent - 1;
 });
 btn1.addEventListener("click", function () {
   num.textContent = 0;
@@ -15,7 +15,7 @@ btn2.addEventListener("click", function () {
   num.textContent = Number(num.textContent) + 1;
 });
 btn3.addEventListener("click", function () {
-  num.textContent = Number(num.textContent) - 10;
+    num.textContent = Number(num.textContent) - 10;
 });
 btn4.addEventListener("click", function () {
   num.textContent = Number(num.textContent) + 10;
