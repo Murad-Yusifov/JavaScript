74080760ffbd981813885e4990a5e32e67f0aab9
Murad-Yusifov <muradmy-azmp203@code.edu.az>
2025-04-04 13:03:47 +0400

    commit it

--- Note Task/note.js
+++ Note Task/note.js
@@ -3,21 +3,21 @@ const action = document.querySelector(".action");
 const color = document.querySelectorAll(".color");
 const noteBox = document.querySelector(".noteBox");
 
-const colors =action.querySelectorall
-
-let dissplay = document.createElement("input")
-
+color.forEach(colorDiv => {
+    colorDiv.addEventListener("click", () => {
+        
+    })
+}
+)
 
 add.addEventListener('click', () => {
     let div = document.createElement("div")
     let deleteBtn = document.createElement("div")
+    let dissplay = document.createElement("textarea")
     deleteBtn.innerText = "x"
     div.classList.add("a")
     deleteBtn.classList.add("b")
     dissplay.classList.add("c")
-    deleteBtn.addEventListener('click', () => {
-        noteBox.appendChild(div)
-    })
     deleteBtn.addEventListener('click', () => {
         noteBox.removeChild(div)
     })
@@ -26,3 +26,36 @@ add.addEventListener('click', () => {
     div.appendChild(deleteBtn)
 
 })
+
+
+// const add = document.getElementById("add");
+// const action = document.querySelector(".action");
+// const colors = document.querySelectorAll(".color");
+// const noteBox = document.querySelector(".noteBox");
+// let selectedColor = "white";
+
+// colors.array.forEach((colorDiv) => {
+//   colorDiv.addEventListener("click", () => {
+//     selectedColor = colorDiv.style.backgroundColor;
+//   });
+// });
+
+// add.addEventListener("click", () => {
+//   let div = document.createElement("div");
+//   let deleteBtn = createElement("button");
+//   let dissplay = document.createElement("input");
+
+//   deleteBtn.innerText = "x";
+//   div.classList.add("a");
+//   deleteBtn.classList.add("b");
+//   dissplay.classList("c");
+//   div.style.backgroundColor = selectedColor;
+
+//   deleteBtn.addEventListener("click", () => {
+//     div.remove();
+//   });
+//   div.appendChild(dissplay)
+//   div.appendChild(deleteBtn)
+//   noteBox.appendChild(div)
+// });
+
--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # JavaScript
 All JavaScript codes will be added here on this repo
+
+
+    <div class="action">
+        <div class="color" style="background-color: green;"></div>
+        <div class="color" style="background-color: yellow;"></div>
+    </div>
+    <button id="add">Add Note</button>
+    <div class="noteBox"></div>
+
+    <script>
+        const add = document.getElementById("add");
+        const action = document.querySelector(".action");
+        const colors = action.querySelectorAll(".color");
+        const noteBox = document.querySelector(".noteBox");
+        let selectedColor = "white";
+
+        colors.forEach(colorDiv => {
+            colorDiv.addEventListener("click", () => {
+                selectedColor = colorDiv.style.backgroundColor;
+            });
+        });
+
+        add.addEventListener('click', () => {
+            let div = document.createElement("div");
+            let deleteBtn = document.createElement("div");
+            let dissplay = document.createElement("input");
+
+            deleteBtn.innerText = "x";
+            div.classList.add("a");
+            deleteBtn.classList.add("b");
+            dissplay.classList.add("c");
+            div.style.backgroundColor = selectedColor;
+
+            deleteBtn.addEventListener('click', () => {
+                div.remove();
+            });
+
+            div.appendChild(dissplay);
+            div.appendChild(deleteBtn);
+            noteBox.appendChild(div);
+        });
+    </script>
+
